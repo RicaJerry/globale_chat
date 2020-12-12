3bae8c9ba69aae30225980ae6e230747de9ae8c0
Rica <rica@zup.ee>
2020-12-12 16:37:19 +0300

    socket ready withoud db

--- server.js
+++ server.js
@@ -1,7 +1,32 @@
 const express = require('express');
+const bodyParser = require('body-parser');
 var app = express();
+var http= require('http').Server(app);
+var io = require("socket.io")(http);
 
 app.use(express.static(__dirname));
-var server = app.listen(3000, () => {
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}));
+
+var messages = [
+    {name: 'Test 2', message: 'msg'},
+    {name: 'Test 3', message: 'msg'}
+]
+
+app.get('/messages', (req, res) => {
+    res.send(messages)
+})
+
+app.post('/messages', (req, res) => {
+    messages.push(req.body);
+    io.emit('message', req.body);
+    res.sendStatus(200);
+})
+
+io.on('connection', (socket) => {
+    console.log("user connected");
+})
+
+var server = http.listen(3000, () => {
     console.log(`server on `, server.address().port);
 })
